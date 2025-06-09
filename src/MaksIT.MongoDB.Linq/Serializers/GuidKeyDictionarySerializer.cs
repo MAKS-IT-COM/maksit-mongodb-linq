e5898dbfe7fca892d8c01f212405a937edb67b2a
Maksym Sadovnychyy <maksym.sadovnychyy@gmail.com>
2025-06-09 22:51:58 +0200

    (feature): Dictionary<Guid, List<Guid>> serializer

---
+++ src/MaksIT.MongoDB.Linq/Serializers/GuidKeyDictionarySerializer.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+
+namespace MaksIT.MaksIT.MongoDB.Linq.Serializers;
+
+public class GuidKeyDictionarySerializer<TValue> : SerializerBase<Dictionary<Guid, TValue>> {
+  public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, Dictionary<Guid, TValue> value) {
+    context.Writer.WriteStartDocument();
+    foreach (var kvp in value) {
+      context.Writer.WriteName(kvp.Key.ToString()); // Convert Guid key to string
+      BsonSerializer.Serialize(context.Writer, kvp.Value);
+    }
+    context.Writer.WriteEndDocument();
+  }
+
+  public override Dictionary<Guid, TValue> Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args) {
+    var dictionary = new Dictionary<Guid, TValue>();
+    context.Reader.ReadStartDocument();
+    while (context.Reader.ReadBsonType() != BsonType.EndOfDocument) {
+      var key = Guid.Parse(context.Reader.ReadName());
+      var value = BsonSerializer.Deserialize<TValue>(context.Reader);
+      dictionary.Add(key, value);
+    }
+    context.Reader.ReadEndDocument();
+    return dictionary;
+  }
+}
